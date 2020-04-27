57ac74d3229eb2782ae0fc153948298776e46943
Allison Taofeek <Devallisontaofeek@gmail.com>
2020-04-27 18:49:43 +0100

    Part three

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,17 +6,23 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using AspdotnetcoreContosso.Models;
+using AspdotnetcoreContosso.Data;
+using AspdotnetcoreContosso.SchoolViewModel;
+using Microsoft.EntityFrameworkCore;
 
 namespace AspdotnetcoreContosso.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly SchoolContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(SchoolContext context, ILogger<HomeController> logger)
         {
+            _context = context;
             _logger = logger;
         }
+        private readonly ILogger<HomeController> _logger;
+
 
         public IActionResult Index()
         {
@@ -27,7 +33,18 @@ namespace AspdotnetcoreContosso.Controllers
         {
             return View();
         }
-
+        public async Task<ActionResult> About()
+        {
+            IQueryable<EnrollmentDateGroup> data =
+                from student in _context.Students
+                group student by student.EnrollmentDate into dateGroup
+                select new EnrollmentDateGroup()
+                {
+                    EnrollmentDate = dateGroup.Key,
+                    StudentCount = dateGroup.Count()
+                };
+            return View(await data.AsNoTracking().ToListAsync());
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
