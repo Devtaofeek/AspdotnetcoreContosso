817f7a6171d7dcb9efd43704ca9f761e0f4f1f5e
Allison Taofeek <Devallisontaofeek@gmail.com>
2020-05-01 03:38:13 +0100

    Part5

--- Migrations/20200501021244_ComplexDataModel.Designer.cs
+++ Migrations/20200501021244_ComplexDataModel.Designer.cs
@@ -4,14 +4,16 @@ using AspdotnetcoreContosso.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AspdotnetcoreContosso.Migrations
 {
     [DbContext(typeof(SchoolContext))]
-    partial class SchoolContextModelSnapshot : ModelSnapshot
+    [Migration("20200501021244_ComplexDataModel")]
+    partial class ComplexDataModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
